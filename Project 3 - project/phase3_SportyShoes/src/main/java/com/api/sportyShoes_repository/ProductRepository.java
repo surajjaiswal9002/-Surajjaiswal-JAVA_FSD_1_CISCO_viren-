2a17e6ef8bb1d086ad0a8c1b05490ca6e0a952c1
surajjaiswal9002 <46620339+surajjaiswal9002@users.noreply.github.com>
2021-10-01 00:44:22 +0530

    Not used

--- Project 3 - project/phase3_SportyShoes/src/main/java/com/api/sportyShoes_repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.sportyShoes_repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.api.sportyShoes_model.Product;
-
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-
-}
