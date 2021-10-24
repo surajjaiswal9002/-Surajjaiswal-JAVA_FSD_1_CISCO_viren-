463dca7e59c9504b1541320626688d91c817b81e
surajjaiswal9002 <46620339+surajjaiswal9002@users.noreply.github.com>
2021-10-24 12:59:42 +0530

    Online test Application
    Online test application pdf ,rar file , write-up doc , screenshot doc

---
+++ Online Test Application/Onlline Test App/js/questions.js
@@ -0,0 +1,115 @@
+// <!-- Suraj jaiswal code -->
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following command is used to create a react app?",
+    answer: "npm install -g create-react-app",
+    options: [
+      "install -g create-react-app",
+      "npm install create-react-app",
+      "npm install -g create-react-app",
+      "None of the Above"
+    ]
+  },
+    {
+    numb: 2,
+    question: ".......... port is the default where the webpack-dev-server will run.",
+    answer: "8080",
+    options: [
+      "3030",
+      "8080",
+      "3306",
+      "3000"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In React what is used to pass data to a component from outside??",
+    answer: "props",
+    options: [
+      "setState",
+      "render with arguments",
+      "props",
+      "PropTypes"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How can you access the state of a component from inside of a member function??",
+    answer: "Methods",
+    options: [
+      "Methods",
+      "Injected",
+      "Both 1 & 2",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is a react.js in MVC?",
+    answer: "Controller",
+    options: [
+      "Middleware",
+      "Controller",
+      "Model",
+      "Router"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "A state in React.js is also known is ..............",
+    answer: "An Internal storage of the component",
+    options: [
+      "A permanent storage",
+      "An Internal storage of the component",
+      "An External storage of the component",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "ReactJS uses _____ to increase performance",
+    answer: "Virtual DOM",
+    options: [
+      "Original DOM",
+      "Virtual DOM",
+      "Both 1 & 2",
+      "None of above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the smallest building block of ReactJS?",
+    answer: "components",
+    options: [
+      "props",
+      "elements",
+      "components",
+      "None of the Above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which function is used to change the state of react component??",
+    answer: "this.setState",
+    options: [
+      "this.State{}",
+      "this.setState",
+      "this.setChangeState",
+      "None of the Above"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is Babel?",
+    answer: "A JavaScript Compile",
+    options: [
+      "A JavaScript transpiler",
+      "A JavaScript interpreter",
+      "A JavaScript Compiler",
+      "None of the above"
+    ]
+  },
+  
+];
